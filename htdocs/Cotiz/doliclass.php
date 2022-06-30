0caad8d4673e5f1b58306b141eadd3612f163c07
Nico <nicobranca@gmail.com>
2022-06-30 17:57:32 -0300

    add env

--- htdocs/Cotiz/doliclass.php
+++ htdocs/Cotiz/doliclass.php
@@ -1,7 +1,6 @@
 <?php
-$httpheader =  ['DOLAPIKEY: mAgnV8S8vehYX2Pm3NQ56o46bgXPQu47'];
-//$Root = "http://localhost/dolibarr/api/index.php/";
-$Root = "http://fontv.ar/htdocs/api/index.php/";
+include "env.php";
+
 
 // funcion para crear items en las facturas $n Numero de facturas $Pu precio unitario $desc descripcion   $qty cantidad 
 
---
+++ htdocs/Cotiz/env.php
@@ -0,0 +1,8 @@
+<?php
+
+$httpheader =  ['DOLAPIKEY: mAgnV8S8vehYX2Pm3NQ56o46bgXPQu47'];
+
+//$Root = "http://localhost/dolibarr/api/index.php/";
+$Root = "http://fontv.ar/htdocs/api/index.php/";
+
+?>
