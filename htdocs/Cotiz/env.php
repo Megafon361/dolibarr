d92edb6555f8f34eca07fe1458baa6ccea174172
Megafon361 <108127208+Megafon361@users.noreply.github.com>
2022-06-30 23:33:34 -0300

    Update env.php

--- htdocs/Cotiz/env.php
+++ htdocs/Cotiz/env.php
@@ -1,7 +1,7 @@
 <?php
 $definite=1;
-$httpheader =  ['DOLAPIKEY: mAgnV8S8vehYX2Pm3NQ56o46bgXPQu47'];
-$Root = " http://localhost:8888/dolibarr/htdocs/api/index.php/explorer";
+$httpheader =  ['DOLAPIKEY: '];
+$Root = " http://localhost:8888/dolibarr/htdocs/api/index.php/";
 
 
 
