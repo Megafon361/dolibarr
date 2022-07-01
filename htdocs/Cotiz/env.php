4d62e243b00de7dd36d32039d3df144087b3dcfc
brancan <nicobranca@gmail.com>
2022-06-30 23:36:39 -0300

    gitignore

--- htdocs/Cotiz/env.php
+++ htdocs/Cotiz/env.php
@@ -1,6 +1,6 @@
 <?php
 $definite=1;
-$httpheader =  ['DOLAPIKEY: '];
+$httpheader =  ['DOLAPIKEY: mAgnV8S8vehYX2Pm3NQ56o46bgXPQu47 '];
 $Root = " http://localhost:8888/dolibarr/htdocs/api/index.php/";
 
 
