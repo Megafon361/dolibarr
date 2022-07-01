c45e4cb4f74a8b89aa5fea4b5941f2bc5827630e
Nico <nicobranca@gmail.com>
2022-07-01 17:26:24 -0300

    Update test.php

--- htdocs/Cotiz/test.php
+++ htdocs/Cotiz/test.php
@@ -1,3 +1,26 @@
+
 <?php
-phpinfo();
+include "doliclass.php";
+
+$curl = curl_init();
+$url = $Root."cotizacionesapi/cotizacions?sortfield=t.rowid&sortorder=DEC&limit=100";
+
+
+  curl_setopt($curl, CURLOPT_URL, $url);
+  //curl_setopt($curl, CURLOPT_POST, 1);
+  //curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $httpheader);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+  $cotizaciones = curl_exec($curl);
+  curl_close($curl);
+  
+;
+
+
+
+
+
+$cotizaciones = json_decode($cotizaciones,"1");
+var_dump($cotizaciones)
 ?>
