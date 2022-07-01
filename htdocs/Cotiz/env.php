008f88f0d4281ea74b840356d74d72fd56929a63
brancan <nicobranca@gmail.com>
2022-07-01 00:32:00 -0300

    try to fix gitignore not working

--- htdocs/Cotiz/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-$definite=1;
-$httpheader =  ['DOLAPIKEY: mAgnV8S8vehYX2Pm3NQ56o46bgXPQu47 '];
-//$Root = "http://localhost:8888/dolibarr/htdocs/api/index.php/";
-$Root = "http://localhost:8888/dolibarr/htdocs/api/index.php/";"
-
-
-?>
