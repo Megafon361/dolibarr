33b51fbf794e660a50a6f8805af11ac818fa0c45
Megafon361 <labs@fontv.ar>
2022-12-18 18:47:50 -0300

    cero

--- htdocs/custom/cotizaciones/myStuff/copy env.php
+++
@@ -1,7 +0,0 @@
-<?php
-$definite=1;
-$httpheader =  ['DOLAPIKEY: '];
-//$Root = "http://localhost:8888/dolibarr/htdocs/api/index.php/";
-$Root = "http://localhost:8888/dolibarr/htdocs/api/index.php/";
-//$Root = "https://fontv.online/dolifon/htdocs/api/index.php/";
-?>
